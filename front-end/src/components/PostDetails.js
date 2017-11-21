32fbf4e84d47917de26ad154619dfc047295d907
ccsalazar <ccsalazar200@gmail.com>
2017-11-20 22:13:55 -0800

    test:Sample Post/Comment Details

--- front-end/src/components/PostDetails.js
+++ front-end/src/components/PostDetails.js
@@ -4,6 +4,7 @@ import CommentCard from './CommentCard';
 
 class PostDetails extends Component {
 state ={
+  posts:[],
   comments:[]
 }
   componentDidMount() {
@@ -14,11 +15,27 @@ state ={
       .then((data) => {
         this.setState({comments:data});
       });
+      const url2 = `http://localhost:3001/posts/8xf0y6ziyjabvozdd253nd`;
+      console.log('fetching from url', url2);
+      fetch(url2, { headers: { 'Authorization': 'whatever-you-want' } })
+        .then( (res) => { return(res.json()) })
+        .then((data) => {
+          this.setState({posts:data});
+        });
   }
   render(){
     return(
       <div className="post-details">
-        <Card />
+          <Card
+            id={this.state.posts.id}
+            timestamp={this.state.posts.timestamp}
+            title={this.state.posts.title}
+            body={this.state.posts.body}
+            author={this.state.posts.author}
+            category={this.state.posts.category}
+            voteScore={this.state.posts.voteScore}
+            commentCount={this.state.posts.commentCount}
+          />
         {
           this.state.comments.map((comment,index)=>
           <CommentCard key={index}
